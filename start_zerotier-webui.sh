a7abb5210d214f8e45751148270c8211cf8b09da
imashen <i@imashen.cn>
2024-06-23 16:57:06 +0800

    fix bugs

--- start_zerotier-webui.sh
+++ start_zerotier-webui.sh
@@ -67,4 +67,4 @@ chown -R zerotier-one:zerotier-one /opt/imashen/zerotier-webui
 chmod 0755 /opt/imashen/zerotier-webui/webui
 
 unset ZEROTIER_WEBUI_PASSWD
-gosu zerotier-one:zerotier-one webui
+gosu zerotier-one:zerotier-one /opt/imashen/zerotier-webui/webui
